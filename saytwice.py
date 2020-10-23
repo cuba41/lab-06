f6ed6b05d310152132fce965c908c6ccfba2387d
Antonio Almaguer <antonio.almaguer@mail.citytech.cuny.edu>
2020-10-23 17:58:04 -0400

    Add files via upload

---
+++ saytwice.py
@@ -0,0 +1,2 @@
+word = input("Give me a word and ill say it twice")
+print(word + word)
---
+++ temp.py
@@ -0,0 +1,4 @@
+
+tempC = input ("Please enter a temperature in celsius")
+tempF = (float(tempC) * (9/5)) +32.0
+print("The temperature is " + str(tempF) + "F")
